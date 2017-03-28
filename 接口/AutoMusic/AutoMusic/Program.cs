b01be7461d248042f0669e5816373432c4d74a32
kangcy <kangcy@axon.com.cn>
2017-03-28 10:54:55 +0800

    aa

--- 接口/AutoMusic/AutoMusic/Program.cs
+++ 接口/AutoMusic/AutoMusic/Program.cs
@@ -12,8 +12,12 @@ using SubSonic.Repository;
 
 namespace AutoMusic
 {
+
+
     class Program
     {
+        static Object locker = new Object();
+
         static void Main(string[] args)
         {
             EGT_OTA.Models.Repository.UpdateDB();
@@ -22,62 +26,70 @@ namespace AutoMusic
             {
                 var MusicStartIndex = Tools.SafeInt(System.Web.Configuration.WebConfigurationManager.AppSettings["MusicStartIndex"]);
                 var MusicEndIndex = Tools.SafeInt(System.Web.Configuration.WebConfigurationManager.AppSettings["MusicEndIndex"]);
+                var LineCount = Tools.SafeInt(System.Web.Configuration.WebConfigurationManager.AppSettings["LineCount"]);
 
-                Thread thread = new Thread(delegate()
+                for (var i = 0; i < LineCount; i++)
                 {
-                    while (MusicStartIndex < MusicEndIndex)
+                    Thread thread = new Thread(delegate()
                     {
-                        try
+                        while (MusicStartIndex < MusicEndIndex)
                         {
-                            var json = HttpHelper.Get("http://music.163.com/api/song/detail?ids=%5B" + MusicStartIndex + "%5D");
-
-                            JArray array = JArray.Parse(JObject.Parse(json)["songs"].ToString());
-                            if (array.Count > 0)
+                            lock (locker)
                             {
-                                JObject model = JObject.Parse(array[0].ToString());
-                                var musicId = model["id"].ToString();
-                                var musicName = model["name"].ToString();
-                                if (!string.IsNullOrWhiteSpace(musicName))
+                                try
                                 {
-                                    var musicUrl = model["mp3Url"].ToString();
-                                    var artistsArray = JArray.Parse(model["artists"].ToString());
-                                    var artists = JObject.Parse(artistsArray[0].ToString());
-                                    var artistsName = artists["name"].ToString();
-                                    var album = JObject.Parse(model["album"].ToString());
-                                    var albumName = album["name"].ToString();
-                                    var musicPicUrl = album["picUrl"].ToString();
+                                    var json = HttpHelper.Get("http://music.163.com/api/song/detail?ids=%5B" + MusicStartIndex + "%5D");
 
-                                    Music music = new Music();
-                                    music.Author = artistsName;
-                                    music.Cover = musicPicUrl;
-                                    music.FileUrl = musicUrl;
-                                    music.Name = musicName;
-                                    music.Number = musicId;
-                                    db.Add<Music>(music);
+                                    JArray array = JArray.Parse(JObject.Parse(json)["songs"].ToString());
+                                    if (array.Count > 0)
+                                    {
+                                        JObject model = JObject.Parse(array[0].ToString());
+                                        var musicId = model["id"].ToString();
+                                        var musicName = model["name"].ToString();
+                                        if (!string.IsNullOrWhiteSpace(musicName))
+                                        {
+                                            var musicUrl = model["mp3Url"].ToString();
+                                            var artistsArray = JArray.Parse(model["artists"].ToString());
+                                            var artists = JObject.Parse(artistsArray[0].ToString());
+                                            var artistsName = artists["name"].ToString();
+                                            var album = JObject.Parse(model["album"].ToString());
+                                            var albumName = album["name"].ToString();
+                                            var musicPicUrl = album["picUrl"].ToString();
 
-                                    Console.WriteLine(MusicStartIndex + ":成功");
+                                            Music music = new Music();
+                                            music.Author = artistsName;
+                                            music.Cover = musicPicUrl;
+                                            music.FileUrl = musicUrl;
+                                            music.Name = musicName;
+                                            music.Number = musicId;
+                                            db.Add<Music>(music);
+                                            Console.WriteLine(MusicStartIndex + ":成功");
+                                        }
+                                        else
+                                        {
+                                            Console.WriteLine(MusicStartIndex + ":失败");
+                                        }
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine(MusicStartIndex);
+                                    }
+
                                 }
-                                else
+                                catch (Exception ex)
                                 {
-                                    Console.WriteLine(MusicStartIndex + ":失败");
+                                    Console.WriteLine(MusicStartIndex + ":" + ex.Message);
                                 }
+
+                                MusicStartIndex += 1;
                             }
-                            else
-                            {
-                                Console.WriteLine(MusicStartIndex);
-                            }
+                            Thread.Sleep(1000);
                         }
-                        catch (Exception ex)
-                        {
-                            Console.WriteLine(MusicStartIndex + ":" + ex.Message);
-                        }
-                        MusicStartIndex += 1;
-                        Thread.Sleep(1000);
-                    }
-                });
-                thread.IsBackground = true;
-                thread.Name = "同步音乐接口线程";
-                thread.Start();
+                    });
+                    thread.IsBackground = true;
+                    thread.Name = "同步音乐接口线程" + i;
+                    thread.Start();
+                }
             }
             catch (Exception ex)
             {
