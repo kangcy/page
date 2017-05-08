aa4bdada0c345eae5ee09eb8bc011b8566eb916b
kangcy <kangcy@axon.com.cn>
2017-05-08 16:21:24 +0800

    aa

--- 接口/EGT_OTA/App_Start/WebApiConfig.cs
+++ 接口/EGT_OTA/App_Start/WebApiConfig.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace EGT_OTA
 {
@@ -13,6 +15,11 @@ namespace EGT_OTA
         public static void Register(HttpConfiguration config)
         {
             config.MapHttpAttributeRoutes();
+
+            //支持跨域
+            //这是重点，从配置文件的appsettings节点中读取跨域的地址
+            var cors = new EnableCorsAttribute(ConfigurationManager.AppSettings["origins"], "*", "*");
+            config.EnableCors(cors);
 
             config.Routes.MapHttpRoute(
                 name: "Api",
